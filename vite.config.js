1914dcfb6d9d2ffeecd7e3a6c55b02216e6d25e1
harshdutta26 <harshdutta2004@gmail.com>
2024-12-05 15:14:14 +0530

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:5000', // Forward API requests to Express
+      '/api': 'https://localhost:5000', // Forward API requests to Express
     },
   },
 });
