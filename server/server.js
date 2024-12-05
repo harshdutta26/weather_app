5640915277a2793bb98280739b0aa988bf7ebf89
harshdutta26 <harshdutta2004@gmail.com>
2024-12-05 16:34:10 +0530

    link updated

--- server/server.js
+++ server/server.js
@@ -11,17 +11,17 @@ app.use(express.json());
 app.get("/api/timenode", async function(req,res){
    try { const inputval=req.query;
     // const lUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${inputval}&limit=1&appid=40f4f989dc2ca4888010ed73d0c78e8a`;
-    const lUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${inputval}&limit=1&appid=${api_key}`;
+    const lUrl = `//api.openweathermap.org/geo/1.0/direct?q=${inputval}&limit=1&appid=${api_key}`;
     const lresponse=await fetch(lUrl);
     const ldata=await lresponse.json();
 
     // const { lat, long } = req.query;
-    const apiUrl = `https://api.timezonedb.com/v2.1/get-time-zone?key=${timeapi_key}&format=xml&by=position&lat=${lat}&lng=${long}`;
+    const apiUrl = `//api.timezonedb.com/v2.1/get-time-zone?key=${timeapi_key}&format=xml&by=position&lat=${lat}&lng=${long}`;
     const response = await fetch(apiUrl);
     const data = await response.json();
 
     // OpenWeatherMap API URL for weather data
-    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api_key}&units=metric`;
+    const weatherUrl = `//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api_key}&units=metric`;
     const response= await fetch(apiUrl);
     const data= await response.json();
     
